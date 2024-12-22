a6756541044f9378b40d7e21369a9bfef0ec1c31
pipil75 <aprilraphaella75@gmail.com>
2024-12-22 17:19:59 +0100

    fichier app

--- app/_app.js
+++ app/_app.js
@@ -1,37 +1,39 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { useEffect } from "react";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-WNJMXB6W');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager */}
-      </Head>
-      <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WNJMXB6W"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Initialisation de Google Analytics
+    const handleRouteChange = (url) => {
+      window.gtag("config", "G-274LZXS5W8", {
+        page_path: url,
+      });
+    };
+
+    // Chargement du script Google Analytics
+    const script1 = document.createElement("script");
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-D59W60T63Z";
+    script1.async = true;
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-D59W60T63Z');
+    `;
+    document.head.appendChild(script2);
+
+    // Suivi des changements de page
+    const handleRouteComplete = (url) => handleRouteChange(url);
+    window.addEventListener("routeChangeComplete", handleRouteComplete);
+
+    return () => {
+      window.removeEventListener("routeChangeComplete", handleRouteComplete);
+    };
+  }, []);
+
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
