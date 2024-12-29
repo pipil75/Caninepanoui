840ee2f989d6c75186b5ea867c770f695cec6da6
pipil75 <aprilraphaella75@gmail.com>
2024-12-29 10:04:18 +0100

    mise a jour message

--- app/message/user/page.jsx
+++ app/message/user/page.jsx
@@ -91,7 +91,7 @@ export default function UserMessages() {
     const messageData = {
       message: reply[conversationId].trim(),
       senderId: auth.currentUser.uid,
-      senderName: auth.currentUser.senderEmail || "Utilisateur",
+      senderName: user.name,
       timestamp: new Date().toISOString(),
     };
 
